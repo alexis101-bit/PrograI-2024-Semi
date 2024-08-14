08dd23a98aadc8db52c0761508dea9335faf4773
alexis101-bit <usss019624@ugb.edu.sv>
2024-08-13 21:57:23 -0600

    Creacion de mi primer proyecto

---
+++ Creacion de mi primer proyecto/Creacion de mi primer proyecto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creacion_de_mi_primer_proyecto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+            Console.ReadLine();
+        }
+    }
+}
