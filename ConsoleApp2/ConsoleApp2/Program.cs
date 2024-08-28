09f34b3980403ae64717f391e98394b6ed31d301
alexis101-bit <usss019624@ugb.edu.sv>
2024-08-28 10:54:42 -0600

    Impuestos Tabla de valoreas

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -6,51 +6,94 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Program
+
+    namespace ImpuestoActividadesEconomicas
     {
-        static void Main(string[] args)
+        class Program
         {
-            double[,] tasasArea = {
-                { 1.00, 0.222395, 0.111111, 0.092903, 0.000152, 0.0000358701, 0.0000092903 }, // Pie Cuadrado a otras unidades
-                { 4.50, 1.00, 0.500004, 0.418061, 0.000684, 0.000161423, 0.0000418061 },      // Vara Cuadrada a otras unidades
-                { 9.00, 2.00002, 1.00, 0.836127, 0.001368, 0.000322846, 0.0000836127 },      // Yarda Cuadrada a otras unidades
-                { 10.7639, 2.392, 1.19599, 1.00, 0.001639, 0.000404686, 0.0001 },            // Metro Cuadrado a otras unidades
-                { 6614.62, 1469.39, 734.695, 610.7, 1.00, 0.246, 0.06107 },                 // Tareas a otras unidades
-                { 27878.4, 6192.77, 3096.39, 2589.99, 4.065, 1.00, 0.247105 },              // Manzana a otras unidades
-                { 107639, 23920, 11959.9, 10000.0, 16.39, 4.04686, 1.00 }                  // Hectárea a otras unidades
-            };
-
-            string[] areas = { "Pie Cuadrado", "Vara Cuadrada", "Yarda Cuadrada", "Metro Cuadrado", "Tareas", "Manzana", "Hectárea" };
-
-            Conversor(tasasArea, areas, "área");
-
-           
-            Console.WriteLine("Presiona cualquier tecla para salir...");
-            Console.ReadKey();
-        }
-
-        static void Conversor(double[,] tasas, string[] unidades, string tipo)
-        {
-            Console.WriteLine($"Conversor de {tipo}");
-            Console.WriteLine("Selecciona una opción:");
-            for (int i = 0; i < unidades.Length; i++)
+            static void Main(string[] args)
             {
-                Console.WriteLine($"{i}: {unidades[i]}");
-            }
-            int origen = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Selecciona la segunda opción:");
-            for (int i = 0; i < unidades.Length; i++)
+                Console.WriteLine("Introduce el monto de la actividad económica:");
+                double monto = double.Parse(Console.ReadLine());
+
+                double impuesto = CalcularImpuesto(monto);
+                Console.WriteLine($"El impuesto a pagar es: {impuesto:C2}"); 
+
+                
+                Console.WriteLine("Presiona cualquier tecla para salir...");
+                Console.ReadKey();
+            }
+
+            static double CalcularImpuesto(double monto)
             {
-                Console.WriteLine($"{i}: {unidades[i]}");
+                double impuesto = 0;
+
+                if (monto >= 0.01 && monto <= 500)
+                {
+                    impuesto = monto * 1.5 / 100;
+                }
+                else if (monto >= 500.01 && monto <= 1000)
+                {
+                    impuesto = ((monto - 500) * 1.5 / 100) + 3;
+                }
+                else if (monto >= 1000.01 && monto <= 2000)
+                {
+                    impuesto = ((monto - 1000) * 3 / 100) + 3;
+                }
+                else if (monto >= 2000.01 && monto <= 3000)
+                {
+                    impuesto = ((monto - 2000) * 6 / 100) + 3;
+                }
+                else if (monto >= 3000.01 && monto <= 6000)
+                {
+                    impuesto = ((monto - 3000) * 9 / 100) + 2;
+                }
+                else if (monto >= 8000.01 && monto <= 18000)
+                {
+                    impuesto = ((monto - 8000) * 15 / 100) + 2;
+                }
+                else if (monto >= 18000.01 && monto <= 30000)
+                {
+                    impuesto = ((monto - 18000) * 39 / 100) + 2;
+                }
+                else if (monto >= 30000.01 && monto <= 60000)
+                {
+                    impuesto = ((monto - 30000) * 63 / 100) + 1;
+                }
+                else if (monto >= 60000.01 && monto <= 100000)
+                {
+                    impuesto = ((monto - 60000) * 93 / 100) + 0.8;
+                }
+                else if (monto >= 100000.01 && monto <= 200000)
+                {
+                    impuesto = ((monto - 100000) * 125 / 100) + 0.7;
+                }
+                else if (monto >= 200000.01 && monto <= 300000)
+                {
+                    impuesto = ((monto - 200000) * 195 / 100) + 0.6;
+                }
+                else if (monto >= 300000.01 && monto <= 400000)
+                {
+                    impuesto = ((monto - 300000) * 255 / 100) + 0.45;
+                }
+                else if (monto >= 400000.01 && monto <= 500000)
+                {
+                    impuesto = ((monto - 400000) * 300 / 100) + 0.4;
+                }
+                else if (monto >= 500000.01 && monto <= 1000000)
+                {
+                    impuesto = ((monto - 500000) * 340 / 100) + 0.3;
+                }
+                else if (monto >= 1000000.01 && monto <= 99999999)
+                {
+                    impuesto = ((monto - 1000000) * 490 / 100) + 0.18;
+                }
+
+                return Math.Round(impuesto, 2); // Redondear el impuesto a dos decimales
+
             }
-            int destino = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Introduce la cantidad a convertir:");
-            double cantidad = double.Parse(Console.ReadLine());
-
-            double resultado = cantidad * tasas[origen, destino];
-            Console.WriteLine($"{cantidad} {unidades[origen]} = {resultado} {unidades[destino]}\n");
         }
     }
 }
+
